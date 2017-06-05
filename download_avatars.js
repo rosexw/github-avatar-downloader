d40ed28793e5ceb06d372443e22e10b1e30ddb4f
Rose Wan <rwan@mail.com>
2017-06-05 20:47:14 +0000

    added GetRepoContributors

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,11 @@
 var request = require('request');
+var fs = require('fs');
+var RepoContributors = require('./get-repo-contributors');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+
+getRepoContributors("jquery", "jquery", function(error, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});

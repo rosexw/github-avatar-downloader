3f29306212b4e31697c0787e0dac6ea7170b9a8c
Rose Wan <rwan@mail.com>
2017-06-06 02:36:30 +0000

    download avatars finished step 10

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,14 @@
 var request = require('request');
 var fs = require('fs');
-var arg = process.argv;
+let arg = process.argv.slice(2);
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-var GITHUB_USER = "user";
-var GITHUB_TOKEN = "token";
+
+var GITHUB_USER = process.env.GITHUB_USER;
+var GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+console.log('GitHub user: ', GITHUB_USER, 'token: ', GITHUB_TOKEN)
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
@@ -17,22 +20,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
-  request(options, function(err, response, body) {
+  request.get(options, function(err, response, body) {
+    if (err) {
+      console.error (err);
+    }
     var data = JSON.parse(body);
+    //console.log(data)
     cb(err, data);
   });
-
-  // function printAvatarURL (err, response) {
-  //   for ()
-  // }
 }
-
-getRepoContributors("jquery", "jquery", function(error, result) {
-  for (var i=0; i<result.length; i++) {
-    downloadImageByURL(result[i].avatar_url, './avatars/' + result[i].id + '.png');
-  }
-});
 
 function downloadImageByURL (url, filePath) {
+  //console.log('downloading... ', url, filePath)
    request.get(url).pipe(fs.createWriteStream(filePath));
 }
+
+if (!arg[0] || !arg[1] ) {
+  console.log("Errors:", error);
+  throw "Input must be two arguments";;
+}
+
+getRepoContributors(arg[0], arg[1], function(error, result) {
+  for (var i=0; i<result.length; i++) {
+    downloadImageByURL(result[i].avatar_url, './avatars/' + result[i].login + '.png');
+  }
+
+});
